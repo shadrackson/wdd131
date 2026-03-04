8f3a88e85e5d8b74393bbcccfd91594143d1f0f4
shadrackson <sonyango@byupathway.edu>
2026-03-04 18:07:42 +0300

    Added photos and edited some html and css files

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+
+// Sets current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Sets last modified date automatically
+document.getElementById("lastModified").textContent = document.lastModified;
