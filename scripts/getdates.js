8aa51cc4a687fbd4f417752b15c0de284b5d96dd
shadrackson <sonyango@byupathway.edu>
2026-03-14 17:46:06 +0300

    added responsive menus in both css and javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,11 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Sets last modified date automatically
 document.getElementById("lastModified").textContent = document.lastModified;
+
+const button = document.querySelector("#menu-button");
+        const menu = document.querySelector("nav ul");
+
+        button.addEventListener("click", () => {
+            menu.classList.toggle("open");
+
+        });
