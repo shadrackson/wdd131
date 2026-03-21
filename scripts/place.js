ea0a630a6b90dbc03bad1767218a0f7550a5428d
shadrackson <sonyango@byupathway.edu>
2026-03-22 01:18:04 +0300

    edited html, css, javascript and images

---
+++ scripts/place.js
@@ -0,0 +1,27 @@
+
+
+
+// Get the current year
+const year = new Date().getFullYear();
+
+//Insert the year into the footer
+document.querySelector("#currentyear").textContent = year;
+
+// Update last modified date
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = document.lastModified;
+ 
+function calculateWindChill(temp, windSpeed) {
+    return Math.round(
+        13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)
+    );
+}
+
+const temp = parseFloat(document.getElementById('temp').textContent);
+const windSpeed = parseFloat(document.getElementById('wind-speed').textContent);
+
+if (temp <= 10 && windSpeed > 4.8) {
+    document.getElementById('wind-chill').textContent = calculateWindChill(temp, windSpeed) + " °C";
+} else {
+    document.getElementById('wind-chill').textContent = "N/A";
+} 
